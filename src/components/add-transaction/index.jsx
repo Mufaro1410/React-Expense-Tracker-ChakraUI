e285655992157ca63caa21983e008cf459da3ae6
Mufaro1410 <fungurayimufaro@gmail.com>
2024-03-28 21:09:13 +0200

    Added Summary, Transactions and Main functionalities

---
+++ src/components/add-transaction/index.jsx
@@ -0,0 +1,61 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
+
+export default function TransactionForm({ onClose, isOpen }) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New Transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Enter Description</FormLabel>
+              <Input
+                placeholder="Enter Transaction Description"
+                name="description"
+                type="text"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Enter Amount</FormLabel>
+              <Input
+                placeholder="Enter Transaction Amount"
+                name="amount"
+                type="number"
+              />
+            </FormControl>
+            <RadioGroup mt={"5"}>
+              <Radio value="income" colorScheme="blue" name="type">
+                Income
+              </Radio>
+              <Radio value="expense" colorScheme="red" name="type">
+                Expense
+              </Radio>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={"4"} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button>Add</Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
+}
